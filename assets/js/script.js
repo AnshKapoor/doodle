fab1475ec778db080a76beb5bba6fca2e286ef38
AnshKapoor <ansh605@gmail.com>
2019-02-06 13:44:41 +0530

    Corrected js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,38 @@
 var timer;
-$(document).ready(function(){
-    $(".result").on("click",function(){
-     var url = $(this).attr("href");
-     var id = $(this).attr("data-linkId");
-     if(!id){
-         alert("LinkId Not found");
-     }
-     increaseLinkClicks(id,url);
-    });
-    var grid = $(".imageResults");
-    grid.on("layoutComplete",function(){
-        $(".gridItem img").css("visibility","visible");
-    });
-    grid.masonry({
-        itemSelector:".gridItem",
-        columnWidth:200,
-        gutter:5,
-        isInitLayout:false
-    });
-    $("[data-fancybox]").fancybox({
+
+$(document).ready(function() {
+
+
+	$(".result").on("click", function() {
+		
+		var id = $(this).attr("data-linkId");
+		var url = $(this).attr("href");
+
+		if(!id) {
+			alert("data-linkId attribute not found");
+		}
+
+		increaseLinkClicks(id, url);
+
+		return false;
+	});
+
+
+	var grid = $(".imageResults");
+
+	grid.on("layoutComplete", function() {
+		$(".gridItem img").css("visibility", "visible");
+	});
+
+	grid.masonry({
+		itemSelector: ".gridItem",
+		columnWidth: 200,
+		gutter: 5,
+		isInitLayout: false
+	});
+
+
+	$("[data-fancybox]").fancybox({
 
 		caption : function( instance, item ) {
 	        var caption = $(this).data('caption') || '';
@@ -32,36 +46,66 @@ $(document).ready(function(){
 	        }
 
 	        return caption;
+	    },
+	    afterShow : function( instance, item ) {
+	        increaseImageClicks(item.src);
 	    }
 
 
 	});
 
-
 });
-function loadImage(src){
-    var image = $("<img>");
-    image.on("load",function(){
-        $("."+className+"a").append(image);
-        clearTimeout(timer);
-        timer = setTimeout(function(){
-            $(".imageResults").masonry();
-        },500);
-     
-    });
-    image.on("error",function(){
-     $("."+className).remove();
-     $.post("ajax/setBroken.php",{src:src});
-    });
-    image.attr("src",src);
+
+function loadImage(src, className) {
+
+	var image = $("<img>");
+
+	image.on("load", function() {
+		$("." + className + " a").append(image);
+
+		clearTimeout(timer);
+
+		timer = setTimeout(function() {
+			$(".imageResults").masonry();
+		}, 500);
+
+	});
+
+	image.on("error", function() {
+		
+		$("." + className).remove();
+
+		$.post("ajax/setBroken.php", {src: src});
+
+	});
+
+	image.attr("src", src);
 
 }
-function increaseLinkClicks(linkId,url){
-$.post("ajax/updateLinkCount.php",{linkId:linkId}).done(function(result){
-    if(result!=""){
-        alert(result);
-        return;
-    }
-    window.location.href = url;
-});
+
+
+function increaseLinkClicks(linkId, url) {
+
+	$.post("ajax/updateLinkCount.php", {linkId: linkId})
+	.done(function(result) {
+		if(result != "") {
+			alert(result);
+			return;
+		}
+
+		window.location.href = url;
+	});
+
+}
+
+function increaseImageClicks(imageUrl) {
+
+	$.post("ajax/updateImageCount.php", {imageUrl: imageUrl})
+	.done(function(result) {
+		if(result != "") {
+			alert(result);
+			return;
+		}
+	});
+
 }
